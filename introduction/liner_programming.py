5d97dadf6f67d4405e721c99fccec8c5e7aae9ef
itokun12 <naoya.ito@smartdrive.co.jp>
2018-12-12 18:16:25 +0900

    Solve linear optimization

---
+++ introduction/liner_programming.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+from ortools.linear_solver import pywraplp
+
+def main():
+  solver = pywraplp.Solver('LinerExample',
+                           pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+
+  # Create the variables x and y.
+  x = solver.NumVar(-solver.infinity(), solver.infinity(), 'x')
+  y = solver.NumVar(-solver.infinity(), solver.infinity(), 'y')
+
+  # Constraint 1: x + 2y <= 14.
+  constraint1 = solver.Constraint(-solver.infinity(), 14)
+  constraint1.SetCoefficient(x, 1)
+  constraint1.SetCoefficient(y, 2)
+
+  # Constraint 2: 3x - y >= 0.
+  constraint2 = solver.Constraint(0, solver.infinity())
+  constraint2.SetCoefficient(x, 3)
+  constraint2.SetCoefficient(y, -1)
+
+  # Constraint 3: x - y <= 2.
+  constraint3 = solver.Constraint(-solver.infinity(), 2)
+  constraint3.SetCoefficient(x, 1)
+  constraint3.SetCoefficient(y, -1)
+
+  # Create the objective function, x + y.
+  objective = solver.Objective()
+  objective.SetCoefficient(x, 3)
+  objective.SetCoefficient(y, 4)
+  objective.SetMaximization()
+
+  # Solve the system.
+  solver.Solve()
+  opt_solution = 3 * x.solution_value() + 4 * y.solution_value()
+  print('Number of variables =', solver.NumVariables())
+  print('Number of constraints =', solver.NumConstraints())
+
+  # The value of each variable in the solution.
+  print('Solution:')
+  print('x = ', x.solution_value())
+  print('y = ', y.solution_value())
+
+  # The objective value of the solution.
+  print('Optimal objective value =', opt_solution)
+
+if __name__ == '__main__':
+  main()
